fbecc9ca0b0b7fa1f10cacc08c66ec678f2b25f3
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-12-16 20:30:15 +0300

    Add 1,2,3,5 prolog tasks

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -4,9 +4,49 @@
 	% xor(A, B)
 	% not(A)
 	% equ(A,B)
+	
+
+	truth(A) :- A=true.
+    false(A) :- A=false.
+
+    bool_var(true).
+    bool_var(false).
+
+	not(A) :-
+    	false(A).
+    	
+
+	and(A, B) :-
+    	truth(A),
+    	truth(B).
+
+	or(A, B) :-
+    	truth(A), truth(B);
+    	truth(A), false(B);
+        false(A), truth(B).
+
+	xor(A, B) :-
+    	truth(A), false(B);
+    	false(A), truth(B).
+
+	equ(A, B) :-
+    	truth(A),truth(B);
+    	false(A),false(B).
+
+	get_result(Expression, true) :- Expression, !.
+	get_result(_, false).
+
+	truth_table(A, B, Expression) :-
+		bool_var(A),
+		bool_var(B),
+		write(A), write(' '),
+		write(B), write(' '),
+		get_result(Expression, Result),
+		write(Result),nl,false.
+    	
 % ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
 % truth_table(A,B,and(A,or(A,B))).
 % true true true
-% true fail true
-% fail true fail
-% fail fail fail
+% true false true
+% false true false
+% false false false
--- exercises/prolog/3_quicksort.pl
+++ exercises/prolog/3_quicksort.pl
@@ -1,1 +1,17 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+quicksort([X|Xs],Ys) :-
+  partition(Xs,X,Left,Right),
+  quicksort(Left,Ls),
+  quicksort(Right,Rs),
+  append(Ls,[X|Rs],Ys).
+quicksort([],[]).
+
+partition([X|Xs],Y,[X|Ls],Rs) :-
+  X =< Y, partition(Xs,Y,Ls,Rs).
+partition([X|Xs],Y,Ls,[X|Rs]) :-
+  X > Y, partition(Xs,Y,Ls,Rs).
+partition([],Y,[],[]).
+
+append([],Ys,Ys).
+append([X|Xs],Ys,[X|Zs]) :- append(Xs,Ys,Zs).
