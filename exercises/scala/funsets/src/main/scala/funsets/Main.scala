81ad87c1cc935cab6019b94b461770a0f55f3339
Zhanna <0305zhanna@mail.ru>
2018-12-17 15:27:57 +0300

    added scala ex

--- exercises/scala/funsets/src/main/scala/funsets/Main.scala
+++ exercises/scala/funsets/src/main/scala/funsets/Main.scala
@@ -1,6 +1,14 @@
 package funsets
 
 object Main extends App {
+
   import FunSets._
+
   println(contains(singletonSet(1), 1))
+  println(forall(Set(2), Set(1, 2, 3)))
+  printSet(union(Set(1, 2, 3), Set(4, 5, 6)))
+  printSet(intersect(Set(1, 2, 4, 3), Set(4, 5, 6)))
+  printSet(diff(Set(1, 2, 4, 3), Set(4, 5, 6)))
+  printSet(filter(Set(1, 2, 4, 3), Set(4, 3)))
+  println(exists(Set(1, 2, 4, 3), Set(3)))
 }
