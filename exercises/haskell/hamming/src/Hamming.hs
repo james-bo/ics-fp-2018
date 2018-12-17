5b652211c7289f74d84a2e2b2c052edc911d161c
kokarovtsevv <44277122+kokarovtsevv@users.noreply.github.com>
2018-12-17 20:14:24 +0300

    Update Hamming.hs

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,5 +1,6 @@
 module Hamming (distance) where
-
 distance :: String -> String -> Maybe Int
-distance la lb = if length la /= length lb then Nothing
+
+distance la lb = 
+  if length la /= length lb then Nothing
   else Just (length (filter id (zipWith (/=) la lb)))
