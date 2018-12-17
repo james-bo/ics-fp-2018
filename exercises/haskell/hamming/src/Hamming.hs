5dce61a9ab148d040e594b04f7ca33e48c9f8ce9
Mohamed Mahdi <lilmed143@gmail.com>
2018-12-17 19:44:48 +0300

    Scala, Haskell implemented

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,8 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance [] [] = Just 0
+distance _ _ = Nothing
+distance (h:xs) (m:ys)
+	| h /= m = fmap (1 +)(distance xs ys)
+	| otherwise = distance xs ys
