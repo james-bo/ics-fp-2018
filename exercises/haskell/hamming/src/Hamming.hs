7a1f0ed06961265e0fa6499e7be24005c5834035
vslchnk <vasilchenko_aleksandr@outlook.com>
2018-12-22 16:06:37 +0300

    added haskell exercise

--- exercises/haskell/hamming/src/Hamming.hs
+++ exercises/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,13 @@
 module Hamming (distance) where
 
-distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance :: String -> String -> Int
+
+distance xs ys
+    | length xs /= length ys = error("lists are not the same length")
+    | otherwise = equalChar 0 xs ys
+        where equalChar n xs ys = if xs == [] && ys == []
+                           then n
+                           else
+                              if (head xs) /= (head ys)
+                                   then equalChar (n + 1) (tail xs) (tail ys)
+                                   else equalChar n (tail xs) (tail ys)
