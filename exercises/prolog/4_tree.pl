ba504152d068d1f30cd9df8927c2b4f6ec4794d2
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-12-22 22:13:56 +0300

    Add prolog 4 task

--- exercises/prolog/4_tree.pl
+++ exercises/prolog/4_tree.pl
@@ -3,3 +3,39 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+% Quicksort
+quicksort([X|Xs],Ys) :-
+  partition(Xs,X,Left,Right),
+  quicksort(Left,Ls),
+  quicksort(Right,Rs),
+  append(Ls,[X|Rs],Ys).
+quicksort([],[]).
+
+partition([X|Xs],Y,[X|Ls],Rs) :-
+  X =< Y, partition(Xs,Y,Ls,Rs).
+partition([X|Xs],Y,Ls,[X|Rs]) :-
+  X > Y, partition(Xs,Y,Ls,Rs).
+partition([],Y,[],[]).
+
+append([],Ys,Ys).
+append([X|Xs],Ys,[X|Zs]) :- append(Xs,Ys,Zs).
+
+% Tree
+
+split(List, Left, Right):-
+    append(Left, Right, List),
+    length(List, Len),
+    HalfLen is Len div 2,
+    length(Left, HalfLen).
+
+balanced_tree_impl(List, instant(Root, LeftChild, RightChild)):-
+    split(List, LeftList, [Root|RightList]),
+    balanced_tree_impl(LeftList, LeftChild),
+    balanced_tree_impl(RightList, RightChild).
+
+balanced_tree(L,T):-
+	quicksort(L, SL),
+	balanced_tree_impl(SL,T).
+
+balanced_tree_impl([], empty).
