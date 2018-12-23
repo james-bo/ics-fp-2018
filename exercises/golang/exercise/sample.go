aff6e8adb72cff4d809c3655c2359d08c67e5af8
Maksim Popov <popovmk97@gmail.com>
2018-12-24 02:05:21 +0300

    go task

--- exercises/golang/exercise/sample.go
+++ exercises/golang/exercise/sample.go
@@ -2,12 +2,59 @@ package exercise
 
 import (
 	"github.com/dikderoy/imagen/drawer"
-	log "github.com/sirupsen/logrus"
 	"image/color"
+	"sync"
 )
 
-
-func NewMandelbrot(iterations int, scaleFactor float32, offsetX, offsetY float32) drawer.MandelbrotGenerator {
-	//start here
+type Mandelbrot struct {
+	iterations		int
+	scaleFactor		float32
+	offsetX 		float32
+	offsetY 		float32
 }
 
+func NewMandelbrot(iterations int, scaleFactor float32, offsetX, offsetY float32) drawer.MandelbrotGenerator {
+	return Mandelbrot{iterations, scaleFactor, offsetX, offsetY}
+}
+
+func (m Mandelbrot) Generate(canvas *drawer.Image) error {
+	for yy := 0; yy < canvas.Height; yy++ {
+		y := (float32(yy)/float32(canvas.Height)-0.5)/m.scaleFactor - m.offsetY
+		for xx := 0; xx < canvas.Width; xx++ {
+			x := (float32(xx)/float32(canvas.Width)-0.5)/m.scaleFactor - m.offsetX
+			canvas.Set(xx, yy, m.GetColor(x, y))
+		}
+	}
+	return nil
+}
+
+func (m Mandelbrot) GenerateParallel(canvas *drawer.Image) error {
+	wg := sync.WaitGroup{}
+	for yy := 0; yy < canvas.Height; yy++ {
+		y := (float32(yy)/float32(canvas.Height)-0.5)/m.scaleFactor - m.offsetY
+		for xx := 0; xx < canvas.Width; xx++ {
+			x := (float32(xx)/float32(canvas.Width)-0.5)/m.scaleFactor - m.offsetX
+			wg.Add(1)
+			go func(xx, yy int) {
+				canvas.Set(xx, yy, m.GetColor(x, y))
+				wg.Done()
+			}(xx, yy)
+		}
+	}
+	return nil
+}
+
+
+func (m Mandelbrot) GetColor(x, y float32) color.Color {
+	z := float32(0)
+	w := float32(0)
+	for i := 0; i < m.iterations; i++ {
+		zz := z*z - w*w + x
+		w = 2.0*z*w + y
+		z = zz
+		if z*z+w*w > 4 {
+			return color.YCbCr{255 - 15*uint8(i), 255 - 15*uint8(i), 15 * uint8(i)}
+		}
+	}
+	return color.Black
+}
