d29c81374125140cea55be4d6cc593848b6287fe
WorkingHandGuard\Protosaider <workinghandguard@gmail.com>
2018-12-25 09:39:56 +0300

    Scala task done

--- exercises/prolog/5_merge.pl
+++ exercises/prolog/5_merge.pl
@@ -1,11 +1,10 @@
 %% определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
 % определяет список R, составленный из этих элементов
 
-append([],L,L). 
-append([H|T],L2,[H|L3])  :-  append(T,L2,L3).
+%% append([],L,L). 
+%% append([H|T],L2,[H|L3])  :-  append(T,L2,L3).
 
-add(A,B,[A|B]).
-
+%% add(A,B,[A|B]).
 
 %% mrg([H1|T1], [H2|T2], Result) :- H1 =< H2, append(H1, Result, Result), mrg(T1, [H2|T2], Result), print('h1<h2').
 %% mrg([H1|T1], [H2|T2], Result) :- append(H2, Result, Result), mrg([H1|T1], T2, Result), print('h2>h1').
--- exercises/scala/funsets/src/main/scala/funsets/Main.scala
+++ exercises/scala/funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,40 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+
+  println("union")
+  println(union(singletonSet(1), singletonSet(2))(0))
+  println(union(singletonSet(1), singletonSet(2))(1))
+  println(union(singletonSet(1), singletonSet(2))(2))
+
+  println("intersect")
+  println(intersect(union(singletonSet(1), singletonSet(2)), singletonSet(2))(0))
+  println(intersect(union(singletonSet(1), singletonSet(2)), singletonSet(2))(1))
+  println(intersect(union(singletonSet(1), singletonSet(2)), singletonSet(2))(2))
+
+  println("diff")
+  println(diff(union(singletonSet(1), singletonSet(2)), singletonSet(2))(0))
+  println(diff(union(singletonSet(1), singletonSet(2)), singletonSet(2))(1))
+  println(diff(union(singletonSet(1), singletonSet(2)), singletonSet(2))(2))
+
+  println("filter")
+  println(filter(union(singletonSet(1), singletonSet(2)), singletonSet(2))(0))
+  println(filter(union(singletonSet(1), singletonSet(2)), singletonSet(2))(1))
+  println(filter(union(singletonSet(1), singletonSet(2)), singletonSet(2))(2))
+
+  println("forall")
+  println(forall(singletonSet(0), union(singletonSet(1), singletonSet(2)))
+  println(forall(singletonSet(1), union(singletonSet(1), singletonSet(2)))
+  println(forall(singletonSet(2), union(singletonSet(1), singletonSet(2)))
+
+  println("exists")
+  println(exists(singletonSet(0), union(singletonSet(1), singletonSet(2)))
+  println(exists(singletonSet(1), union(singletonSet(1), singletonSet(2)))
+  println(exists(singletonSet(2), union(singletonSet(1), singletonSet(2)))
+
+  println("map")
+  println(map(union(singletonSet(1), singletonSet(3)), i => i + 1)(1))
+  println(map(union(singletonSet(1), singletonSet(3)), i => i + 1)(2))
+  println(map(union(singletonSet(1), singletonSet(3)), i => i + 1)(3))
+  println(map(union(singletonSet(1), singletonSet(3)), i => i + 1)(4))
 }
