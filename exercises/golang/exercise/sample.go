6a4bff7a87246f8cc28fc9d45dcb71cf260eb0e3
Mohamed Mahdi <lilmed143@gmail.com>
2018-12-25 23:11:01 +0300

    Go implem

--- exercises/golang/exercise/sample.go
+++ exercises/golang/exercise/sample.go
@@ -2,6 +2,7 @@ package exercise
 
 import (
 	"github.com/dikderoy/imagen/drawer"
+	log "github.com/sirupsen/logrus"
 	"image/color"
 	"math"
 	"sync"
@@ -23,84 +24,96 @@ func (fig Mandelbrot) Generate(canvas *drawer.Image) error {
 
 	iMax := float64(100)
 
-	for ia := 0; ia < canvas.Width; ia++ {
-
-		var x, y float32
-
-		for ip := 0; ip < canvas.Height; ip++ {
+	for ia := 0; ia < canvas.Width; ia ++ {
+		var x float32
+		var y float32
+		
+		for ip := 0; ip < canvas.Height; ip ++ {
 
 			cx := (fig.scaleFactor * float32(ia)) - fig.offsetX
 			cy := (fig.scaleFactor * float32(ip)) - fig.offsetY
 
-			i := 0
 			x = cx
 			y = cy
+			i := 0
 
 			for i < fig.iterations {
-				x, y = x * x - y * y + cx, 2 * x * y + cy
 
+				x, y = x * x - y * y + cx, 2 * x * y + cy
+	
 				if math.Abs(float64(x)) > iMax &&  math.Abs(float64(y)) > iMax {
+					
 					break
 				}
+
 				i++
 			}
 
 			if i < fig.iterations {
+				
 				canvas.Set(ia, ip, color.White)
-			} 
-			else {
+			} else {
+				
 				canvas.Set(ia, ip, color.Black)
 			}
-			
 		}
-
-
 	}
+	
 	return nil
-
+	
 }
 
 func (fig Mandelbrot) GenerateParallel(canvas *drawer.Image) error {
-	
+
 	iMax := float64(100)
+
+	
+
 	var wg sync.WaitGroup
 	wg.Add(canvas.Width)
+	
+	
+	for ia := 0; ia < canvas.Width; ia ++ {
 
-	for ia := 0; ia < canvas.Width; ia++ {
-		var x, y float32
+		var x float32
+		var y float32
 
 		go func(ia int) {
-			for ip := 0; ip < canvas.Height; ip++ {
-				
+
+			for ip := 0; ip < canvas.Height; ip ++ {
+
 				cx := (fig.scaleFactor * float32(ia)) - fig.offsetX
 				cy := (fig.scaleFactor * float32(ip)) - fig.offsetY
 
-				i := 0
 				x = cx
 				y = cy
+				i := 0
 
 				for i < fig.iterations {
-					x, y = x * x - y * y + cx, 2 * x * y + cy
 
+					x, y = x * x - y * y + cx, 2 * x * y + cy
+	
 					if math.Abs(float64(x)) > iMax &&  math.Abs(float64(y)) > iMax {
+						
 						break
 					}
+
 					i++
 				}
 
 				if i < fig.iterations {
+					
 					canvas.Set(ia, ip, color.White)
-				} 
-				else {
+				} else {
+					
 					canvas.Set(ia, ip, color.Black)
 				}
-				
-			}
+			} 
 			wg.Done()
 		}(ia)
 	}
+
 	wg.Wait()
 	return nil
-
+	
 }
-
