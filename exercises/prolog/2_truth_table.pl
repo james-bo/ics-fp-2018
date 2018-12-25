75bd89c80397f76f4c6e65610262babd93dfaf5c
greg <greg.tuzov@gmail.com>
2018-12-26 00:30:59 +0300

    2

--- exercises/prolog/2_truth_table.pl
+++ exercises/prolog/2_truth_table.pl
@@ -10,3 +10,27 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(fail).
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+not(A) :- \+A.
+xor(A, B) :- or(and(A, not(B)), and(not(A), B)).
+equ(A, B) :- not(xor(A, B)).
+
+calc(Exp, true) :- Exp, !.
+calc(_, fail).
+
+printTruthTable(A, B, Res) :- write(A),
+  write('\t'),
+  write(B),
+  write('\t'),
+  writeln(Res).
+
+  truth_table(A,B,Exp) :- bool(A),
+  bool(B),
+  calc(Exp,Res),
+  printTruthTable(A,B,Res),
+  fail.
